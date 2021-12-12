8cf84c590dc02ae17ee4bd80db2585aef0f47a4f
chagy <chagy_x_@hotmail.com>
2021-12-12 15:50:29 +0700

    37

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,16 +3,19 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
 	"github.com/chagy/learning-go-web/pkg/config"
 	"github.com/chagy/learning-go-web/pkg/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
